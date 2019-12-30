5384a80965965c1e15ec609f6fe826c5dad82e8a
Sam <sanoziec@gmail.com>
2019-12-30 16:21:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -24,12 +24,22 @@ $(function() {
     //loading in images
     herodata.forEach(col => {
         col.forEach(img => {
+<<<<<<< HEAD
             var element = `<img data-img-col = ${imgCol} data-img-row = ${imgRow} class = 'img-fluid hero-img' src = '${img}'/>`;
+=======
+            var element = `<img data-img-col = ${imgCol} data-img-row = ${imgRow} class = 'img-fluid position-absolute hero-img' src = '${img}'/>`;
+>>>>>>> 928af1d... Absolute position for hero elements
             $(".hero-img-col").eq(imgCol - 1).append(element);
+            var top = (imgRow - 1) * 25;
+            $(`img[data-img-col = ${imgCol}][data-img-row = ${imgRow}]`).css("top", `${top}%`);
             imgRow++;
         });
         imgCol++;
+<<<<<<< HEAD
         imgRow = 1;
+=======
+        imgRow = 0;
+>>>>>>> 928af1d... Absolute position for hero elements
     });
 
     //Moving images
@@ -37,13 +47,18 @@ $(function() {
         targets: ".hero-img",
         translateX: -130,
         translateY: -1000,
+<<<<<<< HEAD
         duration: 600000,
+=======
+        duration: 800000,
+>>>>>>> 928af1d... Absolute position for hero elements
         loop: true,
     });
 
 
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutationRecord) {
+<<<<<<< HEAD
             var img = mutationRecord.target;
             var imgCol = $(img).attr("data-img-col");
             if (($(img).offset().top + $(img).outerHeight()) < 0) {
@@ -51,6 +66,17 @@ $(function() {
                 $(`#hero-img-col-${imgCol}`).append($(img));
             }
         });
+=======
+            $(".hero-img").each(function(index) {
+                var img = $(this);
+                if ((img.offset().top + img.outerHeight()) < 0) {
+                    img.remove();
+                    img.next().css("margin-top", `${img.outerHeight()*2}px`);
+                    $(`#hero-img-col-${img.attr("data-img-col")}`).append(img);
+                }
+            })
+        })
+>>>>>>> 928af1d... Absolute position for hero elements
     });
     $(".hero-img").each(function() {
         observer.observe(this, {
