928af1d7aa32d59cc8ee1415a0b2de65222b8bfe
Sam <sanoziec@gmail.com>
2019-12-30 16:21:33 -0600

    Absolute position for hero elements

--- script.js
+++ script.js
@@ -19,16 +19,19 @@ const herodata = [
 ]
 
 $(function() {
-    var imgCol = 1;
-    var imgRow = 1;
+    var imgCol = 1,
+        imgRow = 1;
     //loading in images
     herodata.forEach(col => {
         col.forEach(img => {
-            var element = `<img id = "hero-img-${imgCol}-${imgRow}" class = 'img-fluid hero-img' src = '${img}'/>`;
+            var element = `<img data-img-col = ${imgCol} data-img-row = ${imgRow} class = 'img-fluid position-absolute hero-img' src = '${img}'/>`;
             $(".hero-img-col").eq(imgCol - 1).append(element);
+            var top = (imgRow - 1) * 25;
+            $(`img[data-img-col = ${imgCol}][data-img-row = ${imgRow}]`).css("top", `${top}%`);
             imgRow++;
         });
         imgCol++;
+        imgRow = 0;
     });
 
     //Moving images
@@ -36,18 +39,19 @@ $(function() {
         targets: ".hero-img",
         translateX: -130,
         translateY: -1000,
-        duration: 500000,
+        duration: 800000,
         loop: true,
     });
 
 
-    //if (($(".hero-img").offset().top + $(".hero-img").outerHeight) < 0) {
-
     var observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutationRecord) {
             $(".hero-img").each(function(index) {
-                if (($(this).offset().top + $(this).outerHeight()) < 0) {
-                    $(this).remove();
+                var img = $(this);
+                if ((img.offset().top + img.outerHeight()) < 0) {
+                    img.remove();
+                    img.next().css("margin-top", `${img.outerHeight()*2}px`);
+                    $(`#hero-img-col-${img.attr("data-img-col")}`).append(img);
                 }
             })
         })
